236027b957ef3dc70faa2251c12934ccc8a520e2
Karol Pysiak <karol.pysiak.13@gmail.com>
2022-04-08 15:27:48 +0200

    first not erroring version, PSO and FIPSO

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rand::Rng;
+use rand::{Rng, prelude::ThreadRng};
 
 #[derive(Debug)]
 struct Particle{
@@ -6,11 +6,13 @@ struct Particle{
     velocity: Vec<f64>,
     best_position: Vec<f64>,
     best_score: f64,
+    neighbors: Vec<usize>,
 }
 
-fn initialize_particles(n_particles: usize, n_dim: usize, bounds: Vec<(f64, f64)>) -> Vec<Particle>{
+fn initialize_particles(n_particles: usize, bounds: Vec<(f64, f64)>, target_function: fn(&Vec<f64>) -> f64) -> Vec<Particle>{
     let mut particles: Vec<Particle> = Vec::with_capacity(n_particles);
-    let mut rng = rand::thread_rng();
+    let n_dim: usize = bounds.len();
+    let mut rng: ThreadRng = rand::thread_rng();
     for _ in 0..n_particles{
         let mut position: Vec<f64> = Vec::with_capacity(n_dim);
         for j in 0..n_dim{
@@ -25,15 +27,116 @@ fn initialize_particles(n_particles: usize, n_dim: usize, bounds: Vec<(f64, f64)
                 position: position.clone(),
                 velocity: velocity,
                 best_position: position.clone(),
-                best_score: 1f64,
+                best_score: target_function(&position),
+                neighbors: Vec::new(),
             }
         );
     };
+    for i in 0..n_particles{
+        for j in 0..n_particles{
+            if i != j{
+                particles[i].neighbors.push(j);
+            }
+        }
+    }
     particles
 }
 
+fn _update_best(mut particles: Vec<Particle>, id: usize, target_function: fn(&Vec<f64>) -> f64) -> Vec<Particle>{
+    let new_score = target_function(&particles[id].position);
+    if new_score < particles[id].best_score{
+        particles[id].best_score = new_score;
+        particles[id].best_position = particles[id].position.clone();
+    }
+    particles
+}
+
+fn _full_update(mut particles: Vec<Particle>, id: usize, constriction_coef: f64, acceleration_coef: f64, target_function: fn(&Vec<f64>) -> f64) -> Vec<Particle>{
+    let n_dim = particles[id].position.len();
+    let degree = particles[id].neighbors.len();
+    let mut new_velocity: Vec<f64> = vec![0.0; n_dim];
+    let mut new_position: Vec<f64> = Vec::with_capacity(n_dim);
+    let mut rng = rand::thread_rng();
+    for i in 0..degree{
+        let neigh = particles[id].neighbors[i];
+        let c = rng.gen::<f64>();
+        for d in 0..n_dim{
+            new_velocity[d] += acceleration_coef * c * (particles[neigh].best_position[d] - particles[id].position[d]);
+        }
+    }
+    for d in 0..n_dim{
+        new_velocity[d] = constriction_coef * (particles[id].velocity[d] + (new_velocity[d] / degree as f64));
+        new_position.push(particles[id].position[d] + new_velocity[d]);
+    }
+    particles[id].velocity = new_velocity;
+    particles[id].position = new_position;
+    _update_best(particles, id, target_function)
+}
+
+fn _get_best_neighbor(particles: &Vec<Particle>, id: usize) -> Vec<f64>{
+    let n_neighbors: usize = particles[id].neighbors.len();
+    let first_neighbor: usize = particles[id].neighbors[0];
+    let mut gbest_score: f64 = particles[first_neighbor].best_score.clone();
+    let mut gbest: Vec<f64> = particles[first_neighbor].best_position.clone();
+    for i in 1..n_neighbors{
+        let neighbor: usize = particles[id].neighbors[i];
+        if gbest_score > particles[neighbor].best_score{
+            gbest_score = particles[neighbor].best_score;
+            gbest = particles[neighbor].best_position.clone();
+        }
+    }
+    gbest
+}
+
+fn _single_update(mut particles: Vec<Particle>, id: usize, constroction_coef: f64, nostalgia_coef: f64, social_coef: f64, target_function: fn(&Vec<f64>) -> f64) -> Vec<Particle>{
+    let n_dim = particles[id].position.len();
+    let mut new_velocity: Vec<f64> = Vec::with_capacity(n_dim);
+    let mut new_position: Vec<f64> = Vec::with_capacity(n_dim);
+    let mut rng: ThreadRng = rand::thread_rng();
+    let c1: f64 = rng.gen::<f64>();
+    let c2: f64 = rng.gen::<f64>();
+    let gbest = _get_best_neighbor(&particles, id);
+    for d in 0..n_dim{
+        let nostalgia: f64 = nostalgia_coef * c1 * (particles[id].best_position[d] - particles[id].position[d]);
+        let social: f64 = social_coef * c2 *(gbest[d] - particles[id].position[d]);
+        new_velocity.push(constroction_coef * (particles[id].velocity[d] + nostalgia + social));
+        new_position.push(particles[id].position[d] + new_velocity[d]);
+    }
+    particles[id].velocity = new_velocity;
+    particles[id].position = new_position;
+    _update_best(particles, id, target_function)
+}
+
+fn _get_gbest(particles: &Vec<Particle>) -> (f64, Vec<f64>){
+    let mut gbest = particles[0].best_position.clone();
+    let mut gbest_score = particles[0].best_score;
+    for i in 1..(particles.len()){
+        if gbest_score > particles[i].best_score{
+            gbest_score = particles[i].best_score;
+            gbest = particles[i].best_position.clone();
+        }
+    }
+    (gbest_score, gbest)
+}
+
 fn main() {
-    let mut particles = initialize_particles(1, 2, vec![(-1.0, 1.0), (-2.0, 2.0), (-3.1, 3.6)]);
-    particles[0].best_position[0] = 1000000000000000000f64;
-    println!("{:#?}", particles);
+    let n_iter: usize = 5000;
+    let n_particles: usize = 10;
+    let n_dim: usize = 5;
+    let bounds: Vec<(f64, f64)> = vec![(-30.0, 30.0); n_dim];
+
+    let mut particles = initialize_particles(n_particles, bounds, optlib_testfunc::rosenbrock);
+    println!("{:#?}", particles[0]);
+    println!("{:#?}", _get_best_neighbor(&particles, 0));
+    particles = _single_update(particles, 0, 0.7298, 2.05, 2.05, optlib_testfunc::rosenbrock);
+    println!("{:#?}", particles[0]);
+    // for iter in 0..n_iter{
+    //     for i in 0..n_particles{
+    //         particles = _single_update(particles, i, 0.7298, 2.05, 2.05, optlib_testfunc::rosenbrock);
+    //     }
+    //     if iter%100 == 0{
+    //         println!("{:#?}", _get_gbest(&particles).0);
+    //     } 
+    // }
+    // println!("{:#?}", _get_gbest(&particles));
 }
